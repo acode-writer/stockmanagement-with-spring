ad30c17a0a1cdc7d365fde83ed6e2bb87c1ca821
acode-writer <souhaibou.toure122@gmail.com>
2022-03-22 14:39:26 +0000

    flickr configuration.

---
+++ src/main/java/sn/acodewriter/stockmanagement/service/FlickrService.java
@@ -0,0 +1,10 @@
+package sn.acodewriter.stockmanagement.service;
+
+import com.flickr4java.flickr.FlickrException;
+
+import java.io.InputStream;
+
+public interface FlickrService {
+
+    String savePhoto(InputStream photo, String title) throws FlickrException;
+}
